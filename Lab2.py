5baf097e4bd367854694e72356a69f5dd43e4860
Tomassy <87383589+Tomassy69@users.noreply.github.com>
2025-09-12 14:13:53 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -6,12 +6,12 @@ t = float(input("Enter time: "))
 def calculate_height(t):
     return round(h0 - 1 / 2 * 9.8 * t**2, 1)
     # TODO: Implement this function
-print("Height of the ball at", t, "second =", calculate_height(t), "meters")
+print("Height of the ball at", int(t), "second =", calculate_height(t), "meters")
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 speed = float(20)
-time = float(input("Enter time for car (in seconds):"))
+time = float(input("Enter time for car (in seconds): "))
 def calculate_car_distance(time):
     return round(speed * time, 1)
     # TODO: Implement this function
-print("The car will travel", calculate_car_distance(time), "meters in", time, "seconds.")
+print("The car will travel", int(calculate_car_distance(time)), "meters in", int(time), "seconds.")
